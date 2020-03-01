60eaafbfcd114a944accc6d435688bc0e8ac79ea
Cedric De Brito <cdebrito@nvidia.com>
2020-02-29 16:20:37 -0800

    Add functionality to pan and zoom
    Currently bound to clicks.
    The map will pan and zoom to fit all the locations clicked by the user.
    Note: This is only to test the feature, not the final behaviour.

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 let dotSize = 100;
+let panningSpeed = 0.7;
+let allClicks = [];
 
 // implementation of CustomLayerInterface to draw a pulsing dot icon on the map
 // see https://docs.mapbox.com/mapbox-gl-js/api/#customlayerinterface for more info
@@ -61,7 +63,7 @@ let pulsingDot = {
             this.width,
             this.height
         ).data;
-        
+
         // update debug text
         let debugPane = document.getElementById('debugPane');
         if (debugPane)
@@ -74,6 +76,58 @@ let pulsingDot = {
 
         // return `true` to let the map know that the image was updated
         return true;
+    }
+}
+
+function MoveTo(coordinates)
+{
+    // Handle any number of points
+    function getLon(coord){
+        return coord.map(d => Array.isArray(d)? d[0] : d.lon);
+    }
+    function getLat(coord){
+        return coord.map(d => Array.isArray(d)? d[1] : d.lat);
+    }
+    function getMinY(){
+        return Math.min(...getYs());
+    }
+    function getMaxY(){
+        return Math.max(...getYs());
+    }
+    const minLon = Math.min(...getLon(coordinates));
+    const maxLon = Math.max(...getLon(coordinates));
+    const minLat = Math.min(...getLat(coordinates));
+    const maxLat = Math.max(...getLat(coordinates));
+    const averageLonLat = [
+        (minLon + maxLon)/2,
+        (minLat + maxLat)/2
+    ];
+
+    // Fixed zoom if there is only one point
+    if (coordinates.length === 1)
+    {
+        const zoomLevel = 5;
+        map.flyTo({
+            center: averageLonLat,
+            zoom: zoomLevel,
+            speed: panningSpeed,
+            curve: 0.7,
+            easing(t) {return Math.sin(t * Math.PI / 2);},
+            essential: true
+        });
+    }
+    else
+    {
+        map.fitBounds(
+            [[minLon, minLat],
+            [maxLon, maxLat]],
+            {padding: 50,
+            speed: panningSpeed,
+            curve: 0.7,
+            easing(t) {return Math.sin(t * Math.PI / 2);},
+            essential: true
+            }
+        );
     }
 }
 
@@ -107,7 +161,7 @@ map.on('load', function() {
             ]
         }
     });
-    
+
     map.addLayer({
         'id': 'points',
         'type': 'symbol',
@@ -116,7 +170,7 @@ map.on('load', function() {
             'icon-image': 'pulsing-dot'
         }
     });
-    
+
     // Insert the layer beneath any symbol layer.
     const layers = map.getStyle().layers;
 
@@ -164,11 +218,12 @@ map.on('load', function() {
         },
         labelLayerId
     );
-
 });
 
+map.on('click', e => { allClicks.push({lon : e.lngLat.lng, lat : e.lngLat.lat}); MoveTo(allClicks); } );
+
 // Change the map style depending on the state of the radio buttons
-{     
+{
     function switchMapLayer(layer) {
         let layerId = layer.target.id;
         map.setStyle('mapbox://styles/mapbox/' + layerId);
@@ -176,7 +231,7 @@ map.on('load', function() {
     }
 
     let layerList = document.getElementById('menu');
-    let inputs = layerList.getElementsByTagName('input');    
+    let inputs = layerList.getElementsByTagName('input');
     for (let i = 0; i < inputs.length; i++) {
         inputs[i].onclick = switchMapLayer;
     }
