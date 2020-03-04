c6633b15be7c235b0d5a6c7bcae0a91809f0ba87
Soomin Lee <esoodev@gmail.com>
2020-03-03 22:09:06 -0800

    CSV file validation Windows related error fixed.

--- public/js/classes/DataReader.js
+++ public/js/classes/DataReader.js
@@ -42,8 +42,8 @@ class DataReader {
     readFileAsString(id) {
         return new Promise((resolve, reject) => {
             let file = document.getElementById(id).files[0];
-            if (file.type !== 'text/csv')
-                throw `Cannot read file type ${file.type}. Can only read text/csv files.`;
+            if (file.type !== 'text/csv' || file.type !== 'application/vnd.ms-excel')
+                throw `Cannot read file type ${file.type}. Can only read text/csv or application/vnd.ms-excel files.`;
             var reader = new FileReader();
             reader.onload = event => {
                 this.fileContents.set(id, event.target.result);
