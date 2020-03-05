6759b7da5e984686d7ef5e3bd5ec723e74dba4ce
Cedric De Brito <cdebrito@nvidia.com>
2020-03-04 23:02:44 -0800

    Differentiate datasets
    Load a second dataset
    Display up to three datasets, each with a different colour
    Start the animation with a better framing to avoid long flight to destination

--- public/js/script.js
+++ public/js/script.js
@@ -1,88 +1,93 @@
 'use strict';
 
-let dotSize = 20;
 let panningSpeed = 0.7;
 let allData = [];
 
 // implementation of CustomLayerInterface to draw a pulsing dot icon on the map
 // see https://docs.mapbox.com/mapbox-gl-js/api/#customlayerinterface for more info
-let pulsingDot = {
-    width: dotSize,
-    height: dotSize,
-    data: new Uint8Array(dotSize * dotSize * 4),
+function pulsingDot(col = [255, 100, 100])
+{
+    const dotSize = 20;
+    return {
+        color: col,
+        width: dotSize,
+        height: dotSize,
+        data: new Uint8Array(dotSize * dotSize * 4),
 
-    // get rendering context for the map canvas when layer is added to the map
-    onAdd: function() {
-        let canvas = document.createElement('canvas');
-        canvas.width = this.width;
-        canvas.height = this.height;
-        this.context = canvas.getContext('2d');
-    },
+        // get rendering context for the map canvas when layer is added to the map
+        onAdd: function() {
+            let canvas = document.createElement('canvas');
+            canvas.width = this.width;
+            canvas.height = this.height;
+            this.context = canvas.getContext('2d');
+        },
 
-    // called once before every frame where the icon will be used
-    render: function() {
-        let duration = 1500;
-        let t = (performance.now() % duration) / duration;
+        // called once before every frame where the icon will be used
+        render: function() {
+            let duration = 1500;
+            let t = (performance.now() % duration) / duration;
 
-        let radius = (dotSize / 2) * 0.5;
-        let outerRadius = ((dotSize / 2) - radius) * t + radius;
-        let context = this.context;
+            let radius = (dotSize / 2) * 0.5;
+            let outerRadius = ((dotSize / 2) - radius) * t + radius;
+            let context = this.context;
+            let color = this.color;
 
-        // draw outer circle
-        context.clearRect(0, 0, this.width, this.height);
-        context.beginPath();
-        context.arc(
-            this.width / 2,
-            this.height / 2,
-            outerRadius,
-            0,
-            Math.PI * 2
-        );
-        context.fillStyle = 'rgba(255, 200, 200,' + (1 - t) + ')';
-        context.fill();
+            // draw outer circle
+            context.clearRect(0, 0, this.width, this.height);
+            context.beginPath();
+            context.arc(
+                this.width / 2,
+                this.height / 2,
+                outerRadius,
+                0,
+                Math.PI * 2
+            );
+            context.fillStyle = 'rgba(' + color[0] + ', ' + color[1] + ', ' + color[2] + ',' + (1 - t) + ')';
+            context.fill();
 
-        // draw inner circle
-        context.beginPath();
-        context.arc(
-            this.width / 2,
-            this.height / 2,
-            radius,
-            0,
-            Math.PI * 2
-        );
-        context.fillStyle = 'rgba(255, 100, 100, 1)';
-        context.strokeStyle = 'white';
-        context.lineWidth = 2 + 4 * Math.max(0, 1 - 5*t);
-        context.fill();
-        context.stroke();
+            // draw inner circle
+            context.beginPath();
+            context.arc(
+                this.width / 2,
+                this.height / 2,
+                radius,
+                0,
+                Math.PI * 2
+            );
+            context.fillStyle = 'rgba(' + color[0] + ', ' + color[1] + ', ' + color[2] + ', 1)';
+            context.strokeStyle = 'white';
+            context.lineWidth = 2 + 4 * Math.max(0, 1 - 5*t);
+            context.fill();
+            context.stroke();
 
-        // update this image's data with data from the canvas
-        this.data = context.getImageData(
-            0,
-            0,
-            this.width,
-            this.height
-        ).data;
+            // update this image's data with data from the canvas
+            this.data = context.getImageData(
+                0,
+                0,
+                this.width,
+                this.height
+            ).data;
 
-        // update debug text
-        let debugPane = document.getElementById('debugPane');
-        if (debugPane)
-        debugPane.textContent = 'Debug info: ' +
-                                '\nzoom = ' + map.getZoom().toFixed(2) +
-                                ';\ncentre = ' + map.getCenter().toString()
+            // update debug text
+            let debugPane = document.getElementById('debugPane');
+            if (debugPane)
+            debugPane.textContent = 'Debug info: ' +
+                                    '\nzoom = ' + map.getZoom().toFixed(2) +
+                                    ';\ncentre = ' + map.getCenter().toString()
 
-        // continuously repaint the map, resulting in the smooth animation of the dot
-        map.triggerRepaint();
+            // continuously repaint the map, resulting in the smooth animation of the dot
+            map.triggerRepaint();
 
-        // return `true` to let the map know that the image was updated
-        return true;
+            // return `true` to let the map know that the image was updated
+            return true;
+        }
     }
 }
 
-function MoveTo(dataset)
+function minMaxCoord(dataset)
 {
     if (dataset.length === 0) return;
-    
+
     // Handle any number of points
     function getLon(coord){
         return coord.map(d => Array.isArray(d)? d[0] : ('longitude' in d) ? d.longitude : d.lon);
@@ -100,10 +105,16 @@ function MoveTo(dataset)
     const maxLon = Math.max(...getLon(dataset));
     const minLat = Math.min(...getLat(dataset));
     const maxLat = Math.max(...getLat(dataset));
-    const averageLonLat = [
-        (minLon + maxLon)/2,
-        (minLat + maxLat)/2
-    ];
+
+    return [minLon, maxLon, minLat, maxLat];
+}
+
+function MoveTo(dataset)
+{
+    if (dataset.length === 0) return;
+
+    let minLon, maxLon, minLat, maxLat;
+    [minLon, maxLon, minLat, maxLat] = minMaxCoord(dataset);
 
     map.fitBounds(
         [[minLon, minLat],
@@ -132,40 +143,6 @@ let map = new mapboxgl.Map({
 
 // Add a layer with the pulsing dot
 map.on('load', function() {
-    map.addImage('pulsing-dot', pulsingDot);
-
-    // Debug - REMOVEME
-    function pointOnCircle(angle) {
-        const radius = 15;
-        return [Math.cos(angle) * radius, Math.sin(angle) * radius];
-    }
-
-    map.addSource('points', {
-        type: 'geojson',
-        cluster: false,
-        data: {
-            'type': 'FeatureCollection',
-            'features': [
-                {
-                    'type': 'Feature',
-                    'geometry': {
-                        'type': 'MultiPoint',
-                        'coordinates': [pointOnCircle(0), pointOnCircle(10)] // Debug - REMOVEME
-                    }
-                }
-            ]
-        }
-    });
-
-    map.addLayer({
-        'id': 'points',
-        'type': 'symbol',
-        'source': 'points',
-        'layout': {
-            'icon-image': 'pulsing-dot'
-        }
-    });
-
     // Insert the layer beneath any symbol layer.
     const layers = map.getStyle().layers;
 
@@ -248,11 +225,12 @@ map.on('click', e => {
     }
 }
 
-// Load data and sort by dateStart
+// Load data and sort by dateStart (remove once there is another way to load datasets)
 {
     let xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
+        // dataset 1
         let newData = JSON.parse(this.responseText);
         newData.sort((a,b) =>
             (a.dateStart === b.dateStart) ? 0 :
@@ -260,12 +238,18 @@ map.on('click', e => {
         const numPoints = newData.length;
         const dateRangeMs = [strToMs(newData[0].dateStart), strToMs(newData[numPoints-1].dateStart)];
         let obj = {
-            title : "chicago-assault-aggravated",
+            title : this.responseURL,
             numPoints : numPoints,
             dateRangeMs : dateRangeMs,
             data : newData
         };
         allData.push(obj);
+
+        if (allData.length === 1)
+        {
+            xmlhttp.open("GET", "public/data/chicago-battery-aggravated.json", true);
+            xmlhttp.send();
+        }
       }
     };
     xmlhttp.open("GET", "public/data/chicago-assault-aggravated.json", true);
@@ -273,7 +257,7 @@ map.on('click', e => {
 }
 
 // @Nathan once data is loaded, retrieve then using this function.
-function getDataSets(){ 
+function getDataSets(){
     if(isDataSetsAvailable)
         return dataReader.getJsonResults();
     else throw ("Data sets are not yet available.")
@@ -282,11 +266,58 @@ function getDataSets(){
 // Start the animation
 function startAnimation(allData, totalDesiredRuntimeMs = 3 * 60 * 1000 /*3 min*/)
 {
-    var geojsonData = map.getSource('points')._data;
+    // Start by jumping to the view that fits all the points of all datasets
+    let minLon, maxLon, minLat, maxLat;
+    [minLon, maxLon, minLat, maxLat] = minMaxCoord(concat(allData.map(d => d.data)));
+    map.fitBounds(
+        [[minLon, minLat],
+        [maxLon, maxLat]],
+        {padding: 50,
+        maxZoom: 100,
+        linear: true,
+        animate: false}
+    );
 
-    // Start by panning/zooming to the location of the first point
-    // TODO: find the first point among all datasets
-    MoveTo([[allData[0].data[0].longitude, allData[0].data[0].latitude]])
+    const datasetColours = [
+    [255, 100, 100],
+    [100, 155, 100],
+    [100, 100, 255]
+    ]
+
+    // Create a new image and layer for each dataset
+    for (let dataSetIdx = 0; dataSetIdx < allData.length; ++dataSetIdx)
+    {
+        const name = 'points_' + dataSetIdx;
+
+        map.addImage(name, pulsingDot(datasetColours[dataSetIdx]));
+
+        map.addSource(name, {
+            type: 'geojson',
+            cluster: false,
+            data: {
+                'type': 'FeatureCollection',
+                'features': [
+                    {
+                        'type': 'Feature',
+                        'geometry': {
+                            'type': 'MultiPoint',
+                            'coordinates': []
+                        }
+                    }
+                ]
+            }
+        });
+
+        map.addLayer({
+            'id': name,
+            'type': 'symbol',
+            'source': name,
+            'layout': {
+                'icon-image': name
+            }
+        });
+    }
+
 
     // Compute update frequency from date spread in all datasets
     const earliestDateMs = Math.min(...allData.map(d => d.dateRangeMs[0]));
@@ -303,17 +334,16 @@ function startAnimation(allData, totalDesiredRuntimeMs = 3 * 60 * 1000 /*3 min*/
     var eventIndices = [];
     var virtualTime = earliestDateMs;
     var timer = window.setInterval(function() {
-        
-        // Clear the array on first timer tick
-        if (eventIndices.length === 0) geojsonData.features[0].geometry.coordinates = [];
-        
+
         // Remove old features
         for (let dataSetIdx = 0; dataSetIdx < allData.length; ++dataSetIdx)
         {
+            const layerName = 'points_' + dataSetIdx;
+            let geojsonData = map.getSource(layerName)._data;
             if (typeof displayedEventIndices[dataSetIdx] === 'undefined') displayedEventIndices.push(new Array);
             const currentEventIndices = displayedEventIndices[dataSetIdx];
             const dataset = allData[dataSetIdx].data;
-            
+
             for (let i = currentEventIndices.length - 1; i >= 0; --i)
             {
                 const eventIdx = currentEventIndices[i];
@@ -325,6 +355,7 @@ function startAnimation(allData, totalDesiredRuntimeMs = 3 * 60 * 1000 /*3 min*/
             }
         }
 
+        let activeCoordinates = [];
         let allDataProcessed = true;
         for (let dataSetIdx = 0; dataSetIdx < allData.length; ++dataSetIdx)
         {
@@ -337,6 +368,9 @@ function startAnimation(allData, totalDesiredRuntimeMs = 3 * 60 * 1000 /*3 min*/
             {
                 allDataProcessed = false;
 
+                const layerName = 'points_' + dataSetIdx;
+                let geojsonData = map.getSource(layerName)._data;
+
                 // Since the date array is sorted, we only need to check
                 // the elements at and after 'i' in each array
                 for (; (i < dataset.length) && (strToMs(dataset[i].dateStart) <= virtualTime); ++i)
@@ -345,23 +379,25 @@ function startAnimation(allData, totalDesiredRuntimeMs = 3 * 60 * 1000 /*3 min*/
                         [dataset[i].longitude, dataset[i].latitude]
                     );
                     displayedEventIndices[dataSetIdx].push(i);  // Keep track of what we are displaying
-                    
+
                     // Make sure there is a duration. If not, assign default
                     if (typeof dataset[i].duration === 'undefined')
                     {
                         dataset[i].duration = defaultDurationMs;
                     }
                 }
+                activeCoordinates.push(...geojsonData.features[0].geometry.coordinates);
 
-                // Move the pointer forward for each array
+                // Move the pointer forward for this dataset
                 eventIndices[dataSetIdx] = i;
+
+                // Refresh set of points on map for this dataset
+                map.getSource(layerName).setData(geojsonData);
             }
         }
-        // Refresh set of points on map
-        map.getSource('points').setData(geojsonData);
 
         // Pan-and-zoom to include all points currently displayed
-        MoveTo(geojsonData.features[0].geometry.coordinates);
+        MoveTo(activeCoordinates);
 
         // Advance time
         virtualTime += timeIntervalMs;
