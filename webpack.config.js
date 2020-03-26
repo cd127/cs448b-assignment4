fb986db256ff17e73c2e03ac79ebbbd164dec2ca
Cedric De Brito <cdebrito@nvidia.com>
2020-03-26 16:37:03 -0700

    Remove unnecessary files

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-    entry: './index.js',
-    output: {
-        filename: 'js/bundle.js',
-        path: path.resolve(__dirname, 'public')
-    }
-};
